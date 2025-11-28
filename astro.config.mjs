ffba561ef7ad16b4d94f5a85b2369395d890ffc9
oswalbabaresco <oswaldobabaresco@gmail.com>
2025-11-28 09:31:38 -0400

    Fix: Cambios para githubpage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+// Esto es para el desarrollo local
+// export default defineConfig({});
+
+// Esto es para el despliegue en GitHub Pages
 export default defineConfig({
   site: 'https://obababaresco.github.io',
   base: '/Pixelhada',
