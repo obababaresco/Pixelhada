d6f81d4ae731fd924bbf8a6782f29c505b389b79
oswalbabaresco <oswaldobabaresco@gmail.com>
2025-12-06 12:24:37 -0400

    Fix: cambios para desplegar correctamente

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // Esto es para el desarrollo local
-// export default defineConfig({});
+export default defineConfig({});
 
 // Esto es para el despliegue en GitHub Pages
-export default defineConfig({
-  site: 'https://obababaresco.github.io',
-  base: '/Pixelhada',
-});
+// export default defineConfig({
+//   site: 'https://obababaresco.github.io',
+//   base: '/Pixelhada',
+// });
